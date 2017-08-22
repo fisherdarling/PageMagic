37be2a2fde2f3b01a48c9feeacd938a0c17b5d48
Fisher Darling <fdarlingco@gmail.com>
2017-08-22 21:06:27 +0200

    Attempting to add dialog code.
    Page refreshes on button press, unknown reason.

--- index.js
+++ index.js
@@ -1,75 +1,34 @@
+const fs = require("fs");
+const { dialog } = require("electron").remote;
+const Jimp = require("jimp");
+
 var button = document.getElementById("magic");
 button.addEventListener("click", magic);
 
-var picker = document.getElementById("fileSelector");
-function fileChange(event) {
-    var input = picker.files[0];
-    console.log("Getting Data");
-    var reader = new FileReader();
-    reader.onload = function() {
-        var dataURL = reader.result;
-        console.log("Executing Load");
+var input = document.getElementById("fileSelector");
+// input.addEventListener("click", getFile);
 
-        var output = document.getElementById("beforeImage");
-        output.src = dataURL;
-    };
-    reader.readAsDataURL(input);
+let img;
+
+// $('#fileName').html(this.files[0].name);
+
+console.log("Script Loading...");
+
+function getFile(event) {
+    console.log("Get File");
+
+    dialog.showOpenDialog(function(fileNames) {
+        if (fileNames === undefined) return;
+        var path = fileNames[0];
+        console.log(path);
+        Jimp.read(path, function(err, image) {
+            img = image;
+            input.innerHTML = require("path").basename(path);
+            document.getElementById("beforeImage").src = path;
+        });
+    });
 }
 
-function getData() {
-    var file = document.getElementById("fileSelector").files[0];
-    var nPages = parseInt(document.getElementById("numPages").value);
+function magic() {}
 
-    // console.log(file, nPages);
-
-    if (!file || !nPages) {
-        alert("Please make sure all inputs are filled and have proper data.");
-        console.log("Invalid Data");
-        return null;
-    }
-
-    // alert(!file);
-    // alert(file);
-    // alert(!nPages);
-    // alert(nPages);
-
-    console.log("Returning Files");
-
-    // console.log(file, nPages);
-
-    return { file, nPages };
-}
-
-function magic() {
-    var { file, nPages } = getData();
-    console.log(file, nPages);
-    if (!file || !nPages) return;
-
-    console.log("Creating Image");
-
-    var img = document.getElementById("beforeImage");
-
-    if (img.width < nPages) {
-        alert(
-            "Image width cannot be less than the number of pages. The maximum number of pages for this image is: " +
-                img.width
-        );
-        return;
-    }
-
-    const width = img.width;
-    const height = img.height;
-
-    var preCanvas = document.createElement("canvas");
-    preCanvas.width = width;
-    preCanvas.height = height;
-
-    var preContext = preCanvas.getContext("2d");
-    preContext.drawImage(img, 0, 0);
-
-    //Calculate slice width:
-    //Assume pages > than width.
-    // var sW = width / nPages;
-
-    console.log(width, height /*sW*/);
-}
+// function getFileData
--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-const electron = require('electron')
-const {app, BrowserWindow} = electron
+const electron = require("electron");
+const { app, BrowserWindow } = electron;
 
-console.log("Before On Ready")
+console.log("Before On Ready");
 
-app.on('ready', () => {
-    console.log("Creating Browser Windows")
-    let win = new BrowserWindow({width: 800, height: 600})
-    console.log("Loading index.html")
-    win.loadURL(`file://${__dirname}/index.html`)
-})
+let win = null;
+
+app.on("ready", () => {
+    console.log("Creating Browser Windows");
+
+    win = new BrowserWindow({ width: 800, height: 600 });
+
+    console.log("Loading index.html");
+
+    win.loadURL(`file://${__dirname}/index.html`);
+
+    win.on("closed", function() {
+        win = null;
+    });
+});
+
+app.on("window-all-closed", function() {
+    console.log("App Quitting");
+
+    app.quit();
+});
+
+app.on("browser-window-created", function() {
+    console.log("New Browser Window Created");
+});
